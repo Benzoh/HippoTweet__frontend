f86beb1b2476fb9ae4a6823e092d79c5fb65886a
和田 充弘 <benzoh.wada@gmail.com>
2020-07-18 22:13:53 +0900

    add fonts

--- App.tsx
+++ App.tsx
@@ -1,17 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { View, Text } from 'react-native';
+import { AppLoading, Asset, Font } from 'expo';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
+import fonts from 'app/src/fonts';
+import images from 'app/src/images';
 import useCachedResources from 'app/src/hooks/useCachedResources';
 import useColorScheme from 'app/src/hooks/useColorScheme';
 import Navigation from 'app/src/navigation';
 
-export default function App() {
+interface Props {
+  skipLoadingScreen: Boolean;
+}
+
+export default function App(props: Props) {
   const isLoadingComplete = useCachedResources();
+  console.log({ isLoadingComplete });
+  const { skipLoadingScreen } = props;
   const colorScheme = useColorScheme();
 
-  if (!isLoadingComplete) {
-    return null;
+  async function loadResourcesAsync() {
+    await Asset.loadAsync(Object.keys(images).map(key => images[key]));
+    await Font.loadAsync(fonts);
+  }
+
+  if (!isLoadingComplete && !skipLoadingScreen) {
+    return (
+      <AppLoading startAsync={loadResourcesAsync} onError={error => console.warn(error)} />
+    )
   } else {
     return (
       <SafeAreaProvider>
@@ -20,4 +37,8 @@ export default function App() {
       </SafeAreaProvider>
     );
   }
+}
+
+App.defaultProps = {
+  skipLoadingScreen: false,
 }
