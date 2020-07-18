1d955011a6aa095aeceba3bf0d3af2e50cb1dcba
和田 充弘 <benzoh.wada@gmail.com>
2020-07-18 20:55:32 +0900

    fix paths

--- App.tsx
+++ App.tsx
@@ -2,9 +2,9 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import useCachedResources from 'app/src/hooks/useCachedResources';
+import useColorScheme from 'app/src/hooks/useColorScheme';
+import Navigation from 'app/src/navigation';
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
