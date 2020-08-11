85826b0444acc8544e2412933dca94491211b167
和田 充弘 <benzoh.wada@gmail.com>
2020-08-11 21:20:12 +0900

    add profile image.

--- src/components/pages/Sub/index.tsx
+++ src/components/pages/Sub/index.tsx
@@ -1,14 +1,42 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, TouchableOpacity, AsyncStorage, Button } from 'react-native';
+import { Text, View, Image, StyleSheet, TouchableOpacity, AsyncStorage, Button } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 import { retrieveData, removeData } from 'app/src/lib/localStorage';
+import { COLOR } from 'app/src/constants/theme';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  profileImage: {
+    width: 200,
+    height: 200,
+    borderRadius: 100,
+  },
+  loading: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  screenName: {
+    marginTop: 5,
+    fontSize: 20,
+  },
+  button: {
+    marginTop: 20,
+    width: 120,
+    height: 45,
+    borderRadius: 5,
+    backgroundColor: COLOR.MAIN,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonLabel: {
+    color: COLOR.WHITE,
+    fontSize: 16,
   },
 });
 
@@ -26,18 +54,33 @@ export default function Sub() {
     });
   }, []);
 
+  function largeProfileImage(string: string) {
+    return string.replace('_normal', '');
+  }
+
+  if (!user) {
+    return (
+      <View style={styles.loading}>
+        <Text> Loading... </Text>
+      </View >
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>{user ? '@' + user.screen_name : 'unknown'}</Text>
-      <Button
-        title="Logout"
+      <Image style={styles.profileImage} source={{ uri: largeProfileImage(user.profile_image_url_https) }} />
+      <Text style={styles.screenName}>{user ? '@' + user.screen_name : 'unknown'}</Text>
+      <TouchableOpacity
+        style={styles.button}
         onPress={() =>
           removeData('TWITTER_TOKEN').then(() => {
             setUser(undefined);
             navigate('Login');
           })
         }
-      />
+      >
+        <Text style={styles.buttonLabel}>Logout</Text>
+      </TouchableOpacity>
     </View>
   );
 }
