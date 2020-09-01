c2f20dc54747d6d07e8f8e07680cab413a6a8fe8
和田 充弘 <benzoh.wada@gmail.com>
2020-09-01 21:22:08 +0900

    Wip: "message": "Could not authenticate you."

---
+++ src/lib/_mediaUpload.ts
@@ -0,0 +1,92 @@
+/* eslint-disable @typescript-eslint/camelcase */
+import Constants from 'expo-constants';
+import * as ImageManipulator from 'expo-image-manipulator';
+import HmacSHA1 from 'crypto-js/hmac-sha1';
+import Base64 from 'crypto-js/enc-base64';
+
+// const uploadURL = 'http://localhost:5001/thetweet-5b342/us-central1/mediaUpload';
+const uploadURL = 'https://upload.twitter.com/1.1/media/upload.json';
+// const uploadURL = Constants.manifest.extra.uploadUrl;
+const API_KEY = Constants.manifest.extra.apiKey;
+const API_KEY_SECRET = Constants.manifest.extra.apiKeySecret;
+
+interface Props {
+  auth: {
+    oauth_token: string;
+    oauth_token_secret: string;
+  };
+  uri: string;
+  fileUri: string;
+}
+
+export default async function mediaUpload(props: Props) {
+  console.log('mediaUpload::', { props });
+
+  const base64res = await ImageManipulator.manipulateAsync(props.fileUri, [], { base64: true });
+  // console.log('base64res' + JSON.stringify(base64res));
+
+  const formData = new FormData();
+
+  // formData.append('oauth_token', props.auth.oauth_token);
+  // formData.append('oauth_token_secret', props.auth.oauth_token_secret);
+  formData.append('media_data', base64res.base64);
+  // formData.append('media', { type: 'image/jpeg', uri: props.fileUri, name: props.fileUri.split('/').pop() });
+
+  // console.log({ formData });
+  console.log(JSON.stringify(formData));
+
+  const params = {
+    oauth_consumer_key: API_KEY,
+    oauth_nonce: Math.random().toString(32).substring(2),
+    oauth_signature_method: 'HMAC-SHA1',
+    oauth_timestamp: Math.floor(Date.now() / 1000),
+    oauth_token: props.auth.oauth_token,
+    oauth_version: '1.0',
+  };
+
+  const esc = encodeURIComponent;
+  const requestParams = Object.keys(params)
+    .map(k => esc(k) + '=' + esc(params[k]))
+    .join('&');
+
+  // make signature
+  const signatureKey = encodeURI(API_KEY_SECRET) + '&' + encodeURI(props.auth.oauth_token_secret);
+  const encodedRequestMethod = encodeURI('POST');
+  const encodedRequestUrl = encodeURI(uploadURL);
+  const signatureData = encodedRequestMethod + '&' + encodedRequestUrl + '&' + requestParams;
+  const hash = HmacSHA1(signatureData, signatureKey);
+  const signature = Base64.stringify(hash);
+
+  params.oauth_signature = signature;
+
+  const headerParams = Object.keys(params)
+    .sort()
+    .map(k => esc(k) + '=' + esc(params[k]))
+    .join(', ');
+  console.log({ headerParams });
+
+  const bound_text = '--h-i-p-p-o--------------1ae47d990b354d1b00a4eea60e6b5b72\r\n';
+  const headers = {
+    // Accept: 'application/json',
+    Authorization: 'OAuth ' + headerParams,
+    // 'Content-Type': 'multipart/form-data; boundary=' + bound_text,
+    'Content-Transfer-Encoding': 'base64',
+  };
+  console.log({ headers });
+
+  return await fetch(uploadURL, {
+    method: 'POST',
+    headers: headers,
+    body: base64res.base64,
+  })
+    .then(response => {
+      return response.json();
+    })
+    .then(resJson => {
+      console.log({ resJson });
+      return resJson;
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
--- src/lib/mediaUpload.ts
+++ src/lib/mediaUpload.ts
@@ -7,8 +7,6 @@ import Base64 from 'crypto-js/enc-base64';
 // const uploadURL = 'http://localhost:5001/thetweet-5b342/us-central1/mediaUpload';
 const uploadURL = 'https://upload.twitter.com/1.1/media/upload.json';
 // const uploadURL = Constants.manifest.extra.uploadUrl;
-const API_KEY = Constants.manifest.extra.apiKey;
-const API_KEY_SECRET = Constants.manifest.extra.apiKeySecret;
 
 interface Props {
   auth: {
@@ -18,25 +16,27 @@ interface Props {
   uri: string;
   fileUri: string;
 }
+interface Params {
+  oauth_consumer_key: string;
+  oauth_nonce: string;
+  oauth_signature?: string;
+  oauth_signature_method: 'HMAC-SHA1';
+  oauth_timestamp: number;
+  oauth_token: string;
+  oauth_version: '1.0';
+  media_data?: string;
+}
 
 export default async function mediaUpload(props: Props) {
-  console.log('mediaUpload::', { props });
-
   const base64res = await ImageManipulator.manipulateAsync(props.fileUri, [], { base64: true });
-  // console.log('base64res' + JSON.stringify(base64res));
-
-  const formData = new FormData();
-
-  // formData.append('oauth_token', props.auth.oauth_token);
-  // formData.append('oauth_token_secret', props.auth.oauth_token_secret);
-  formData.append('media_data', base64res.base64);
-  // formData.append('media', { type: 'image/jpeg', uri: props.fileUri, name: props.fileUri.split('/').pop() });
-
-  // console.log({ formData });
-  console.log(JSON.stringify(formData));
-
-  const params = {
-    oauth_consumer_key: API_KEY,
+  // const oauth = {
+  //   consumer_key: Constants.manifest.extra.apiKey,
+  //   consumer_secret: Constants.manifest.extra.apiKeySecret,
+  //   token: props.auth.oauth_token,
+  //   token_secret: props.auth.oauth_token_secret,
+  // };
+  const params: Params = {
+    oauth_consumer_key: Constants.manifest.extra.apiKey,
     oauth_nonce: Math.random().toString(32).substring(2),
     oauth_signature_method: 'HMAC-SHA1',
     oauth_timestamp: Math.floor(Date.now() / 1000),
@@ -44,41 +44,44 @@ export default async function mediaUpload(props: Props) {
     oauth_version: '1.0',
   };
 
+  const e = encodeURI;
   const esc = encodeURIComponent;
   const requestParams = Object.keys(params)
-    .map(k => esc(k) + '=' + esc(params[k]))
+    .map(k => k + '=' + params[k])
     .join('&');
 
   // make signature
-  const signatureKey = encodeURI(API_KEY_SECRET) + '&' + encodeURI(props.auth.oauth_token_secret);
-  const encodedRequestMethod = encodeURI('POST');
-  const encodedRequestUrl = encodeURI(uploadURL);
-  const signatureData = encodedRequestMethod + '&' + encodedRequestUrl + '&' + requestParams;
+  const signatureKey = esc(Constants.manifest.extra.apiKeySecret) + '&' + esc(props.auth.oauth_token_secret);
+  const encodedRequestMethod = esc('POST');
+  const encodedRequestUrl = esc(uploadURL);
+  const signatureData = encodedRequestMethod + '&' + encodedRequestUrl + '&' + esc(requestParams);
+  console.log({ signatureData });
   const hash = HmacSHA1(signatureData, signatureKey);
   const signature = Base64.stringify(hash);
 
-  params.oauth_signature = signature;
+  params.oauth_signature = esc(signature);
+  console.log({ params });
 
+  // make headers
   const headerParams = Object.keys(params)
     .sort()
-    .map(k => esc(k) + '=' + esc(params[k]))
+    .map(k => `${e(k)}="${e(params[k])}"`)
     .join(', ');
   console.log({ headerParams });
 
-  const bound_text = '--h-i-p-p-o--------------1ae47d990b354d1b00a4eea60e6b5b72\r\n';
-  const headers = {
-    // Accept: 'application/json',
-    Authorization: 'OAuth ' + headerParams,
-    // 'Content-Type': 'multipart/form-data; boundary=' + bound_text,
-    'Content-Transfer-Encoding': 'base64',
-  };
-  console.log({ headers });
-
-  return await fetch(uploadURL, {
+  // eslint-disable-next-line no-undef
+  const request = new Request(uploadURL, {
     method: 'POST',
-    headers: headers,
-    body: base64res.base64,
-  })
+    headers: {
+      authorization: 'OAuth ' + headerParams,
+      'Content-Type': 'multipart/form-data; boundary=--hippohack--',
+    },
+    body: 'media_data=',
+    // body: 'media_data=' + base64res.base64,
+  });
+  console.log({ request });
+
+  return await fetch(request)
     .then(response => {
       return response.json();
     })
